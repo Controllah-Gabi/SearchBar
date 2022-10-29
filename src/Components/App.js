27d8a0480fb24e34fb46dec511a3ebfc311310ee
Junior Gabi <juniorgabi@juniors-air.home>
2022-10-29 21:41:21 +0100

    connected to api via axios

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = ()=>{
-    return <div className="ui container" style={{marginTop:'10px'}}><SearchBar/></div>;
-};
+
+class App extends React.Component {
+    async onSearchSubmit(term){
+        const response= await axios.get('https://api.unsplah.com/search/photos',{
+            params: { query: term},
+            headers: {
+                Authorizaton: 'Client-ID xlLdFP_MhrG4gzSUd2zbADPlmR9f8AJnt-21xEd7Fzk'
+            }
+        })
+    }
+    render(){
+    return( <div className="ui container" style={{marginTop:'10px'}}>
+                <SearchBar onSearchSubmit={this.onSearchSubmit}/>
+            </div>)
+    }
+}
 
 export default App;
 
